62f4d51715159c0f9e00ee88ecf061612f40de91
vimniky <vimniky@gmail.com>
2016-10-31 04:33:01 +0800

    Update Devps --> Remove react-markdown add classname

--- src/shared/modules/HomePage/index.js
+++ src/shared/modules/HomePage/index.js
@@ -1,19 +1,8 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { connect } from 'react-redux'
-import Markdown from 'react-markdown'
 import Styles from './Home.css'
 import { selectUsers } from './selectors'
-
-const content = `
-# Hello from Markdown
-
-This is a paragraph rendered using Markdown.
-
-* First Argument
-* Second Argument
-* Third Argument
-`
 
 export function Home({ users }) {
   return (
@@ -22,7 +11,6 @@ export function Home({ users }) {
       <p className={Styles.intro}>
         Produced with ❤️ by <a href="https://github.com/seedyee">seedyee</a>
       </p>
-      <Markdown source={content} />
       <hr />
       <h3> Users </h3>
       {users && users.map((user, i) => (<li key={i}> {user} </li>))}
